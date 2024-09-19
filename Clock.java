1ffa80f4927f957773ec26b0921e6558f4c296e9
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-19 10:56:07 -0700

    clock finished

--- Clock.java
+++ Clock.java
@@ -2,9 +2,39 @@ public class Clock
 {
   public static void main(String[] args)
   {
-    // test code here
+    timeOfDay(0); // midnight
+    timeOfDay(12); // noon
+    timeOfDay(7); // morning
+    timeOfDay(15); // afternoon
+    timeOfDay(21); // evening
+    timeOfDay(18); // dusk
   }
 
-  // write code for timeOfDay here
-
+  public static void timeOfDay(int time)
+  {
+    if (time == 0)
+    { 
+      System.out.println("midnight"); 
+    }
+    else if (time == 12) 
+    {
+      System.out.println("noon"); 
+    }
+    else if (time == 18)
+    {
+      System.out.println("dusk"); 
+    }
+    else if (time > 0 && time < 12)
+    {
+      System.out.println("morning"); 
+    }
+    else if (time > 12 && time < 18)
+    {
+      System.out.println("afternoon"); 
+    }
+    else
+    {
+      System.out.println("evening"); 
+    }
+  }
 }
